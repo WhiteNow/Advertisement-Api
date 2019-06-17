ce2f7c81035dae4c11a9c0231a87f775bfc58472
bryam <bryam.vasquez@tektonlabs.com>
2019-06-17 11:52:36 -0500

    add company and add model

---
+++ db/migrate/20190617161820_create_adds.rb
@@ -0,0 +1,18 @@
+class CreateAdds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :adds do |t|
+      t.references :company, foreign_key: true
+      t.string :title
+      t.string :description
+      t.string :photo_url
+      t.integer :max
+      t.boolean :unlimited
+      t.integer :used
+      t.string :category
+      t.datetime :exp_date
+
+
+      t.timestamps
+    end
+  end
+end
